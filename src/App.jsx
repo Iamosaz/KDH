43b9560ce124f27f74e14bbe3a10b57a5b2a1c16
unknown <75techstore@gmail.com>
2025-07-09 16:06:29 +0100

    Fix casing issue with Topbar import for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Topbar from './Components/Topbar/Topbar';
-import Navbar from './Components/Navbar/Navbar';
+
+// import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home';
+import Topbar from './components/Topbar/Topbar';
 
 const App = () => {
   return (
     <Router>
       {/* Group Topbar and Navbar */}
       <header>
-        <Topbar />
+       <Topbar />
         {/* <Navbar /> */}
       </header>
 
