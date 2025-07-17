3fd5ca821b8da2149dcf6b3d7849d40d079e4297
unknown <75techstore@gmail.com>
2025-07-17 15:10:52 +0100

    HeroSlider fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import HeroSlider from '../Components/HeroSlider/HeroSlider';
 import MissionSection from '../components/MissionSection/MissionSection';
 import JoinMovement from '../components/JoinMovement/JoinMovement';
 import JoinMission from '../components/JoinMission/JoinMission';
-import FooterCTA from '../components/FooterCTA/FooterCTA';
-import FooterBottom from '../components/FooterBottom/FooterBottom';
+import HeroSlider from '../Components/HeroSlider/HeroSlider';
+
+
 
 const Home = () => {
   return (
