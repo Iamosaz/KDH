2e2d03a7919b4f65fc354f0338f18a464f324204
unknown <75techstore@gmail.com>
2025-07-17 14:55:34 +0100

    fixed-topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import FooterBottom from './components/FooterBottom/FooterBottom';
 import FooterCTA from './components/FooterCTA/FooterCTA';
-import Topbar from './components/Topbar/Topbar';
+import Topbar from "./components/Topbar/Topbar";
 
 const App = () => {
   return (
